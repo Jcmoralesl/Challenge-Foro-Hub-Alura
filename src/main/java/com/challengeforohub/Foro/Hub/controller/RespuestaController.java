753b51023964e25cf3841e5c025ac15cd709d22c
Jcmoralesl <juancamilo.morales43@gmail.com>
2025-01-20 19:57:20 -0500

    Terminar creación RepuestaController

--- src/main/java/com/challengeforohub/Foro/Hub/controller/RespuestaController.java
+++ src/main/java/com/challengeforohub/Foro/Hub/controller/RespuestaController.java
@@ -1,12 +1,21 @@
 package com.challengeforohub.Foro.Hub.controller;
 
 
+import com.challengeforohub.Foro.Hub.domain.respuesta.Respuesta;
+import com.challengeforohub.Foro.Hub.domain.respuesta.dto.ActualizarRespuestaDTO;
+import com.challengeforohub.Foro.Hub.domain.respuesta.dto.CrearRespuestaDTO;
+import com.challengeforohub.Foro.Hub.domain.respuesta.dto.DetalleRespuestaDTO;
 import com.challengeforohub.Foro.Hub.domain.respuesta.repository.RespuestaRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/respuestas")
@@ -24,5 +33,103 @@ public class RespuestaController {
     private RespuestaRepository respuestaRepository;
 
     @Autowired
+    List<ValidarRespuestaCreada> crearValidadores;
+
+    @Autowired
+    List<ValidarRespuestaActualizada> actualizarValidadores;
+
+    @PostMapping
+    @Transactional
+    @Operation (summary = "Registra una nueva respuesta en la base de datos, vinculada a un usuario y tema existente.")
+    public ResponseEntity<DetalleRespuestaDTO> crearRespuesta(@RequestBody @Valid CrearRespuestaDTO crearRespuestaDTO,
+                                                              UriComponentsBuilder uriBuilder){
+        crearValidadores.forEach(v -> v.validate(crearRespuestaDTO));
+
+        Usuario usuario = usuarioRepository.getReferenceById(crearRespuestaDTO.usuarioId());
+        Topico topico = topicoRepository.findById(crearRespuestaDTO.topicoId()).get();
+
+        var respuesta = new Respuesta(crearRespuestaDTO, usuario, topico);
+        respuestaRepository.save(respuesta);
+
+        var uri = uriBuilder.path("/respuestas/{id}").buildAndExpand(respuesta.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DetalleRespuestaDTO(respuesta));
+
+    }
+
+    @GetMapping("/topico/{topicoId}")
+    @Operation(summary = "Lee todas las respuestas del tema dado")
+    public ResponseEntity<Page<DetalleRespuestaDTO>>
+    leerRespuestaDeTopico(@PageableDefault(size = 5, sort = {"ultimaActualizacion"},
+            direction = Sort.Direction.ASC) Pageable pageable, @PathVariable Long topicoId){
+        var pagina = respuestaRepository.findAllByTopicoId(topicoId, pageable).map(DetalleRespuestaDTO::new);
+        return ResponseEntity.ok(pagina);
+    }
+
+    @GetMapping("/usuario/{nombreUsuario}")
+    @Operation(summary = "Lee todas las respuestas del nombre de usuario proporcionado.")
+    public ResponseEntity<Page<DetalleRespuestaDTO>>
+    leerRespuestasDeUsuarios(@PageableDefault(size = 5, sort = {"ultimaActualizacion"},
+            direction = Sort.Direction.ASC)Pageable pageable, @PathVariable Long usuarioId){
+        var pagina = respuestaRepository.findAllByUsuarioId(usuarioId, pageable).map(DetalleRespuestaDTO::new);
+        return ResponseEntity.ok(pagina);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Lee una única respuesta por su ID")
+    public ResponseEntity<DetalleRespuestaDTO> leerUnaRespuesta(@PathVariable Long id){
+        Respuesta respuesta = respuestaRepository.getReferenceById(id);
+
+        var datosRespuesta = new DetalleRespuestaDTO(
+                respuesta.getId(),
+                respuesta.getMensaje(),
+                respuesta.getFechaCreacion(),
+                respuesta.getUltimaActualizacion(),
+                respuesta.getSolucion(),
+                respuesta.getBorrado(),
+                respuesta.getUsuario().getId(),
+                respuesta.getUsuario().getUsername(),
+                respuesta.getTopico().getId(),
+                respuesta.getTopico().getTitulo()
+        );
+        return ResponseEntity.ok(datosRespuesta);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    @Operation(summary = "Actualiza el mensaje de la respuesta, la solucion o el estado de la respuesta.")
+    public ResponseEntity<DetalleRespuestaDTO> actualizarRespuesta(@RequestBody @Valid ActualizarRespuestaDTO actualizarRespuestaDTO, @PathVariable Long id){
+        actualizarValidadores.forEach(v -> v.validate(actualizarRespuestaDTO, id));
+        Respuesta respuesta = respuestaRepository.getReferenceById(id);
+        respuesta.actualizarRespuesta(actualizarRespuestaDTO);
+
+        if(actualizarRespuestaDTO.solucion()){
+            var temaResuelto = topicoRepository.getReferenceById(respuesta.getTopico().getId());
+            temaResuelto.setEstado(Estado.CLOSED);
+        }
+
+        var datosRespuesta = new DetalleRespuestaDTO(
+                respuesta.getId(),
+                respuesta.getMensaje(),
+                respuesta.getFechaCreacion(),
+                respuesta.getUltimaActualizacion(),
+                respuesta.getSolucion(),
+                respuesta.getBorrado(),
+                respuesta.getUsuario().getId(),
+                respuesta.getUsuario().getUsername(),
+                respuesta.getTopico().getId(),
+                respuesta.getTopico().getTitulo()
+        );
+        return ResponseEntity.ok(datosRespuesta);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    @Operation(summary = "Elimina una respuesta por su Id")
+    public ResponseEntity<?> borrarRespuesta(@PathVariable Long id){
+        Respuesta respuesta = respuestaRepository.getReferenceById(id);
+        respuesta.eliminarRespuesta();
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
